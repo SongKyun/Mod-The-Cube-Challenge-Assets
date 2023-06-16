030159bc9f19192b86f88d94c84cbe693d970772
Song-Kyun <89376275+SongKyun@users.noreply.github.com>
2023-06-16 22:11:28 +0900

    23.6.16
    불투명도 미해결..

--- Assets/ModTheCube/Cube.cs
+++ Assets/ModTheCube/Cube.cs
@@ -5,19 +5,25 @@ using UnityEngine;
 public class Cube : MonoBehaviour
 {
     public MeshRenderer Renderer;
-    
+    public float opacity = 0.3f; // 불투명도 값
+
     void Start()
     {
-        transform.position = new Vector3(3, 4, 1);
-        transform.localScale = Vector3.one * 1.3f;
-        
-        Material material = Renderer.material;
-        
-        material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
+        transform.position = new Vector3(0, 5, 0);
+        transform.localScale = Vector3.one * 3.0f;
+
+        Material material = Renderer.sharedMaterial;
+
+        Color color = material.color;
+        color = Color.yellow; // 색상을 먼저 설정
+        color.a = opacity; // 알파 값을 변경
+        material.color = color;
+
+        Renderer.material = material; // 수정된 머티리얼을 다시 할당
     }
-    
+
     void Update()
     {
-        transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
+        transform.Rotate(0.0f, 20.0f * Time.deltaTime, 0.0f);
     }
 }
